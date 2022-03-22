d63d9b3df83dd40ab65ac434d3be582e1e0c46b1
Leo Hao <lhao@macquarietelecom.com>
2022-03-23 08:44:38 +1100

    feat: add triplet sum

---
+++ src/leetcode/triplets_sum.py
@@ -0,0 +1,41 @@
+'''
+https://medium.com/javarevisited/100-days-to-amazon-day-1-b9e07228f079
+
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+Find all unique triplets in the array which gives the sum of zero
+
+'''
+
+from typing import List
+
+
+class Solution:
+  @staticmethod
+  def three_sum(arr: List[int]) -> List[List[int]]:
+    target = 0
+    ans = []  # for answer list
+    ls: List[str] = []  # a string list for checking unique format: "a:b:c"
+
+    arr.sort()  # sort ascending
+
+    l = r = 0
+    nums = len(arr)
+
+    for i in range(0, nums - 2):
+      l = i + 1    # pick from left
+      r = nums - 1  # pick from right
+
+      while l < r:
+        if arr[i] + arr[l] + arr[r] == target:
+          s = f'{arr[i]}:{arr[l]}:{arr[r]}'
+          if s not in ls:
+            ans.append([arr[i], arr[l], arr[r]])
+            ls.append(s)
+          l += 1
+          r -= 1
+        elif arr[i] + arr[l] + arr[r] > target:
+          r -= 1
+        else:
+          l += 1
+
+    return ans
---
+++ tests/test_triplets_sum.py
@@ -0,0 +1,14 @@
+import pytest
+
+from leetcode.triplets_sum import Solution
+
+
+@pytest.mark.parametrize('arr,ans', [
+  ([-2, -1, 1, 0], [[-1, 0, 1]]),
+  ([-2, -1, 1, 2, 3, -3, 0], [[-2, 0, 2], [-1, 0, 1], [-3, 0, 3], [-2, -1, 3], [-3, 1, 2]]),
+])
+def test_triplets_sum(arr, ans):
+  res = Solution.three_sum(arr)
+  assert len(res) == len(ans)
+  for a in ans:
+    assert a in res
