dd4a7ea23a2835a31b9e3bb2504ef4daa8adbf2b
Leo Hao <lhao@macquarietelecom.com>
2022-04-05 13:23:25 +1000

    refactor: improve unique paths algorithm

--- src/leetcode/unique_paths.py
+++ src/leetcode/unique_paths.py
@@ -27,8 +27,23 @@ Constraints:
 
 
 class Solution:
+
+  @staticmethod
+  def unique_paths_dp(m: int, n: int) -> int:
+    dp = [[0] * n for _ in range(m)]
+
+    for i in range(m):
+      for j in range(n):
+        if i == 0 and j == 0:
+          dp[i][j] = 1
+        else:
+          dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+    return dp[m - 1][n - 1]
+
   @staticmethod
   def unique_paths(m: int, n: int) -> int:
+    '''this method will timeout when dealing with relatively large grid'''
 
     def rec(row, col) -> int:
       if row < 0 or col < 0:
--- tests/test_unique_paths.py
+++ tests/test_unique_paths.py
@@ -10,3 +10,12 @@ from leetcode.unique_paths import Solution
 def test_unique_paths(m, n, ans):
   res = Solution.unique_paths(m, n)
   assert res == ans
+
+
+@pytest.mark.parametrize('m,n,ans', [
+  (3, 7, 28),
+  (3, 2, 3),
+])
+def test_unique_paths_dp(m, n, ans):
+  res = Solution.unique_paths_dp(m, n)
+  assert res == ans
