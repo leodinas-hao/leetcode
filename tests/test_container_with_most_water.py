7a9b97b4ca4b8d1809054c0b2b9be069a3b7919e
Leo Hao <lhao@macquarietelecom.com>
2022-04-05 12:36:26 +1000

    feat: add container with most water

---
+++ tests/test_container_with_most_water.py
@@ -0,0 +1,13 @@
+import pytest
+
+from leetcode.container_with_most_water import Solution
+
+
+@pytest.mark.parametrize('height,ans', [
+  ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+  ([1, 1], 1),
+  ([1, 2, 4, 3], 4)
+])
+def test_container_with_most_water(height, ans):
+  res = Solution.max_area(height)
+  assert ans == res
